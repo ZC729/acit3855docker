60dfbddde692ec9e4dc30086479e6e547d1af4e8
Zachary Chang <64169591+ZC729@users.noreply.github.com>
2021-11-25 11:33:50 -0800

    Update app.py

--- storage/app.py
+++ storage/app.py
@@ -22,14 +22,14 @@ from base import Base
 from order import Order
 from update import Update
 
-# if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
-#     print("In Test Environment")
-#     app_conf_file = "/config/app_conf.yml"
-#     log_conf_file = "/config/log_conf.yml"
-# else:
-#     print("In Dev Environment")
-app_conf_file = "app_conf.yml"
-log_conf_file = "log_conf.yml"
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
 
 with open(app_conf_file, 'r') as f:
     app_config = yaml.safe_load(f.read())
@@ -40,8 +40,8 @@ with open(log_conf_file, 'r') as f:
 
 logger = logging.getLogger('basicLogger')
 
-# logger.info("App Conf File: %s" % app_conf_file)
-# logger.info("Log Conf File: %s" % log_conf_file)
+logger.info("App Conf File: %s" % app_conf_file)
+logger.info("Log Conf File: %s" % log_conf_file)
 
 DB_ENGINE = create_engine(f"mysql+pymysql://{app_config['datastore']['user']}:{app_config['datastore']['password']}@{app_config['datastore']['hostname']}:{app_config['datastore']['port']}/{app_config['datastore']['db']}", pool_pre_ping=True, pool_recycle=300, pool_size=10, max_overflow=20, pool_timeout=3600)
 Base.metadata.bind = DB_ENGINE
