269ff0bd900d37075eae306a099628801d47f581
Zachary Chang <64169591+ZC729@users.noreply.github.com>
2021-12-01 16:51:54 -0800

    Update app.py

--- audit_log/app.py
+++ audit_log/app.py
@@ -99,7 +99,7 @@ def get_order(index):
     return { "message": "Not Found"}, 404
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-app.add_api("ZCACIT3855-Inventory-API-1.0.0-swagger.yaml", base_path-"/audit_log", strict_validation=True, validate_responses=True)
+app.add_api("ZCACIT3855-Inventory-API-1.0.0-swagger.yaml", base_path="/audit_log", strict_validation=True, validate_responses=True)
 if "TARGET_ENV" not in os.environ or os.environ["TARGET_ENV"] != "test": 
     CORS(app.app) 
     app.app.config['CORS_HEADERS'] = 'Content-Type'
