ec3133cb48551059f979979f53bf607b48a1677d
Zachary Chang <64169591+ZC729@users.noreply.github.com>
2021-12-01 16:33:52 -0800

    Update app.py

--- audit_log/app.py
+++ audit_log/app.py
@@ -100,8 +100,9 @@ def get_order(index):
 
 app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api("ZCACIT3855-Inventory-API-1.0.0-swagger.yaml", base_path-"/audit_log", strict_validation=True, validate_responses=True)
-CORS(app.app)
-app.app.config['CORS_HEADERS'] = 'Content-Type'
+if "TARGET_ENV" not in os.environ or os.environ["TARGET_ENV"] != "test": 
+    CORS(app.app) 
+    app.app.config['CORS_HEADERS'] = 'Content-Type'
 
 if __name__ == "__main__":
     app.run(port=8200)
